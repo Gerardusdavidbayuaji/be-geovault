b554123c75bcb34123e4bb03da0a23aa1efe5cb6
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-02-27 18:41:48 +0700

    refactor: app

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 
-import categoryRoutes from "./routes/categoryRoutes";
+import datasRoutes from "./routes/datasRoutes";
 import uploadRoutes from "./routes/uploadRoutes";
 
 dotenv.config();
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/api", categoryRoutes);
+app.use("/api", datasRoutes);
 app.use("/api", uploadRoutes);
 
 export default app;
